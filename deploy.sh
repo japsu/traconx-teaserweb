aa5869e80f22f86337f95f6475a91f2c4f59fb19
Santtu Pajukanta <santtu@pajukanta.fi>
2013-12-08 00:36:25 +0200

    better deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-exec rsync -avz --delete-after public/ kurikka.tracon.fi:/srv/www/2014.tracon.fi
+set -e
+node_modules/james/bin/james build
+rsync -avz --delete-after public/ kurikka.tracon.fi:/srv/www/2014.tracon.fi
